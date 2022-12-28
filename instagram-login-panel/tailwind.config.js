0d7585f94a38fa07a88eab8522fa6545f7a4ad04
Galina Yesko <gyesko@fmv.local>
2022-12-28 14:25:10 +0300

    instagram login panel cloning with using Tailwindcss

---
+++ instagram-login-panel/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+
+  
+ 
+  theme: {
+    colors: {
+      'dark-blue': '#385185',
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
